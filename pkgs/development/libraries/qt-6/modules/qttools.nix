18d0fe6b5a578e731683d6982d4b8c2444a4095c
Milan Hauth <milahu@gmail.com>
2022-01-30 20:26:31 +0100

    qt6.qttools: rm clang

--- pkgs/development/libraries/qt-6/modules/qttools.nix
+++ pkgs/development/libraries/qt-6/modules/qttools.nix
@@ -3,26 +3,11 @@
 , lib
 , qtbase
 , qtdeclarative
-, clang_13 # build QDoc. clang>=8
-, pkg-config # find clang
 }:
 
 qtModule {
   pname = "qttools";
   qtInputs = [ qtbase qtdeclarative ];
-  buildInputs = [ clang_13 ];
-  nativeBuildInputs = [ pkg-config clang_13 ];
   outputs = [ "out" "dev" "bin" ];
-
-  postConfigure = ''
-    echo clang_13 = ${clang_13}
-    echo "todo debug: Could NOT find Clang"
-    exit 1
-  '';
-
-  cmakeFlags = [
-    "--trace-expand" # debug cmake
-  ];
-
   NIX_CFLAGS_COMPILE = lib.optional stdenv.isDarwin ''-DNIXPKGS_QMLIMPORTSCANNER="${qtdeclarative.dev}/bin/qmlimportscanner"'';
 }
