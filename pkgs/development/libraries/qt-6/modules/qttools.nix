49514b43465ff5873b3d884b5c7a830605391298
Milan Hauth <milahu@gmail.com>
2022-01-30 19:37:03 +0100

    qt6.qttools: add clang 2

--- pkgs/development/libraries/qt-6/modules/qttools.nix
+++ pkgs/development/libraries/qt-6/modules/qttools.nix
@@ -9,7 +9,7 @@
 qtModule {
   pname = "qttools";
   qtInputs = [ qtbase qtdeclarative ];
-  buildInputs = [ clang ];
+  buildInputs = [ clang clang.dev ];
   outputs = [ "out" "dev" "bin" ];
 
   NIX_CFLAGS_COMPILE = lib.optional stdenv.isDarwin ''-DNIXPKGS_QMLIMPORTSCANNER="${qtdeclarative.dev}/bin/qmlimportscanner"'';
