b61ae20ab376212993639142c606d765bd0f07dd
Milan Hauth <milahu@gmail.com>
2022-01-31 23:41:48 +0100

    build with clang 9

--- pkgs/development/python-modules/shiboken6/default.nix
+++ pkgs/development/python-modules/shiboken6/default.nix
@@ -2,7 +2,7 @@
 , python
 , fetchurl
 , lib
-, stdenv
+#, stdenv
 , pyside6
 , cmake
 , qt6
@@ -11,7 +11,10 @@
 
 # sphinx-build - not found! doc target disabled
 
-let llvmPackages = llvmPackages_9; in
+let
+  llvmPackages = llvmPackages_9;
+  stdenv = llvmPackages.stdenv;
+in
 
 stdenv.mkDerivation rec {
   pname = "shiboken6";
@@ -26,21 +29,24 @@ stdenv.mkDerivation rec {
     #./milahu-debug.patch
   ];
 
+#    cp ${./compilersupport.cpp} sources/shiboken6/ApiExtractor/clangparser/compilersupport.cpp
+
   postPatch = ''
-    cp ${./compilersupport.cpp} sources/shiboken6/ApiExtractor/clangparser/compilersupport.cpp
     cd sources/${pname}
   '';
 
   #CLANG_INSTALL_DIR = llvmPackages.libclang.out;
-  CLANG_INSTALL_DIR = "${llvmPackages.libclang.lib}:${llvmPackages.libcxx.dev}"; # /lib/clang/9.0.1/include/
+  #CLANG_INSTALL_DIR = "${llvmPackages.libclang.lib}:${llvmPackages.libcxx.dev}"; # /lib/clang/9.0.1/include/
 
   nativeBuildInputs = [ cmake ];
 
   buildInputs = [
+    /*
     llvmPackages.libllvm
     llvmPackages.clang-unwrapped
     llvmPackages.libclang # ${llvmPackages_9.libclang.lib}/lib/clang/9.0.1/include
     llvmPackages.libcxx # include <type_traits> -> ${llvmPackages_9.libcxx.dev}/include/c++/v1/type_traits
+    */
     # /nix/store/vdfr889lwm84xzgabqhdnm9vwc0xrwy1-libcxx-9.0.1-dev/include/c++/v1/type_traits
     python
     qt6.qtbase
