2c66faf11b3e5933abc2f47e7004f49f9aca11c0
Milan Hauth <milahu@gmail.com>
2022-02-01 20:10:58 +0100

    shiboken clang + pyside clang

--- pkgs/development/python-modules/pyside6/default.nix
+++ pkgs/development/python-modules/pyside6/default.nix
@@ -85,7 +85,7 @@ let
   llvmPackages = llvmPackages_13;
   #llvmPackages = llvmPackages_10;
 
-  #stdenv = llvmPackages.stdenv; # gcc -> clang
+  stdenv = llvmPackages.stdenv; # gcc -> clang
 
   sha256OfQtVersion = {
     pyside6 = {
