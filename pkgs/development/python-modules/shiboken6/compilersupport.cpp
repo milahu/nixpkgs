36d4714064caa455289f12315f69a132cfbc5425
Milan Hauth <milahu@gmail.com>
2022-02-01 17:13:20 +0100

    shiboken compilersupport.cpp: manually add glibc include path

--- pkgs/development/python-modules/shiboken6/compilersupport.cpp
+++ pkgs/development/python-modules/shiboken6/compilersupport.cpp
@@ -400,11 +400,18 @@ static void appendClangBuiltinIncludes(HeaderPaths *p)
 
 
 // milahu debug
-std::cout << "milahu cerr: clangBuiltinIncludesDir: manually add libcxx include path: /nix/store/vdfr889lwm84xzgabqhdnm9vwc0xrwy1-libcxx-9.0.1-dev/include/c++/v1\n";
-
 {
-  //QString clangBuiltinIncludesDir = QLatin1String("/nix/store/vdfr889lwm84xzgabqhdnm9vwc0xrwy1-libcxx-9.0.1-dev/include/c++/v1"); // ${llvmPackages_9.libcxx.dev}/include/c++/v1
-  QString clangBuiltinIncludesDir = QLatin1String("/nix/store/vb1dk5c3xg9r8q6mdw3cl3qgzm6vgnvd-libcxx-10.0.1-dev/include/c++/v1"); // ${llvmPackages_10.libcxx.dev}/include/c++/v1
+  //QString clangBuiltinIncludesDir = QLatin1String("/nix/store/vdfr889lwm84xzgabqhdnm9vwc0xrwy1-libcxx-9.0.1-dev/include/c++/v1"); // ${llvmPackages_9.libcxx.dev}/include/c++/v1/type_traits
+  QString clangBuiltinIncludesDir = QLatin1String("/nix/store/vb1dk5c3xg9r8q6mdw3cl3qgzm6vgnvd-libcxx-10.0.1-dev/include/c++/v1"); // ${llvmPackages_10.libcxx.dev}/include/c++/v1/type_traits
+  std::cerr << "milahu cerr: clangBuiltinIncludesDir: manually add libcxx include path: " << qPrintable(clangBuiltinIncludesDir) << "\n";
+  p->append(HeaderPath{
+    QFile::encodeName(clangBuiltinIncludesDir),
+    HeaderType::System
+  });
+}
+{
+  QString clangBuiltinIncludesDir = QLatin1String("/nix/store/93z3gj6kl0qvdm1mzwb5vaxlz7i481lz-glibc-2.33-62-dev/include"); // ${glibc.dev}/include/features.h
+  std::cerr << "milahu cerr: clangBuiltinIncludesDir: manually add glibc include path: " << qPrintable(clangBuiltinIncludesDir) << "\n";
   p->append(HeaderPath{
     QFile::encodeName(clangBuiltinIncludesDir),
     HeaderType::System
