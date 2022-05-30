94120dbcca6b29e11ec883bf666c5d27067fc9d5
Timothy DeHerrera <tim@nrdxp.dev>
2022-05-30 13:12:15 -0600

    python3Packages.pyqt6: init at 6.3.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -7829,7 +7829,15 @@ in {
 
   pyqt5 = callPackage ../development/python-modules/pyqt/5.x.nix { };
 
+  pyqt6 = callPackage ../development/python-modules/pyqt/6.x.nix { };
+
+  pyqt6_with_qtmultimedia = self.pyqt6.override {
+    withMultimedia = true;
+  };
+
   pyqt5_sip = callPackage ../development/python-modules/pyqt/sip.nix { };
+
+  pyqt6_sip = callPackage ../development/python-modules/pyqt/sip6.nix { };
 
   pyqt5_with_qtmultimedia = self.pyqt5.override {
     withMultimedia = true;
