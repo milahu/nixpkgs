98eb057b8958ece559528b2494de8801c44187fd
Milan Hauth <milahu@gmail.com>
2022-06-18 19:28:30 +0200

    qt6.qtwebengine: fix ninja flags
    honor NIX_BUILD_CORES in recursive ninja calls

--- pkgs/development/libraries/qt-6/modules/qtwebengine.nix
+++ pkgs/development/libraries/qt-6/modules/qtwebengine.nix
@@ -216,6 +216,30 @@ qtModule rec {
 
   requiredSystemFeatures = [ "big-parallel" ];
 
+  # honor NIX_BUILD_CORES in recursive ninja calls
+  # https://bugreports.qt.io/browse/QTBUG-95176
+  # based on ninjaBuildPhase in
+  # pkgs/development/tools/build-managers/ninja/setup-hook.sh
+  # this must run before cmake
+  # to set NINJAFLAGS for qtwebengine/cmake/Functions.cmake
+  preConfigure = ''
+    local buildCores=1
+
+    # Parallel building is enabled by default.
+    if [ "''${enableParallelBuilding-1}" ]; then
+        buildCores="$NIX_BUILD_CORES"
+    fi
+
+    local flagsArray=(
+        -j$buildCores -l$NIX_BUILD_CORES
+        $ninjaFlags "''${ninjaFlagsArray[@]}"
+    )
+
+    # honor NIX_BUILD_CORES in recursive ninja calls
+    export NINJAFLAGS="''${flagsArray[@]}"
+    echo "preConfigure: setting NINJAFLAGS=$NINJAFLAGS"
+  '';
+
   postInstall = ''
     # This is required at runtime
     mkdir $out/libexec
