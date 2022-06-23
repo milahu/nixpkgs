f2e85a7c8e46698ca30f3660aad56f3fc92f5190
Milan Hauth <milahu@gmail.com>
2022-06-24 00:02:13 +0200

    fix nix-build-profiler

--- pkgs/development/python-modules/nix-build-profiler/src/nix_build_profiler.py
+++ pkgs/development/python-modules/nix-build-profiler/src/nix_build_profiler.py
@@ -209,7 +209,8 @@ def print_process_info(process_info, root_pid, file=sys.stdout, depth=0):
   cwd = info["cwd"] # always None
   environ = info["environ"] # always None
   child_procs = len(info["child_pids"])
-  cmdline[0] = os.path.basename(cmdline[0]) # full path is in info["exe"]
+  if len(cmdline) > 0:
+    cmdline[0] = os.path.basename(cmdline[0]) # full path is in info["exe"]
   #if cmdline[0] in {"g++", "gcc"}:
   if cmdline[0] in {"g++", "gcc", "cc1plus", "as"}:
     # make gcc less verbose
