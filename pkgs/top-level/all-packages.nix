52f8393ab4000a270ecc98235f16eb509533ae22
Milan Hauth <milahu@gmail.com>
2022-06-24 17:08:48 +0200

    fix nix-build-profiler

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -34352,7 +34352,7 @@ with pkgs;
 
   nix-simple-deploy = callPackage ../tools/package-management/nix-simple-deploy { };
 
-  nix-build-profiler = ../development/python-modules/nix-build-profiler { };
+  nix-build-profiler = callPackage ../development/python-modules/nix-build-profiler { };
 
   alejandra = callPackage ../tools/nix/alejandra { };
 
