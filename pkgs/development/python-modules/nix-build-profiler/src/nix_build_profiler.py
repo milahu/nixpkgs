04c0db9b8b22a0285e6cf7c6f8777db5ef5d5109
Milan Hauth <milahu@gmail.com>
2022-06-24 19:33:49 +0200

    update nix_build_profiler.py

--- pkgs/development/python-modules/nix-build-profiler/src/nix_build_profiler.py
+++ pkgs/development/python-modules/nix-build-profiler/src/nix_build_profiler.py
@@ -20,7 +20,7 @@ import io
 config_interval = 1
 config_root_process_name = 'nix-daemon'
 
-psutil.cpu_percent() # start monitoring cpu
+#psutil.cpu_percent() # start monitoring cpu
 
 cpu_count = psutil.cpu_count()
 cpu_width = len(str(cpu_count * 100))
@@ -51,66 +51,11 @@ def find_root_process(name):
   #assert len(ls) == 1 # !=1 when build is running
   return ls[0]
 
-last_cpu_time = dict()
-
-def get_process_tree_slow(process, depth=0):
-  """
-  build process tree in depth-first traversal to cumulate:
-  cpu -> sum_cpu
-  mem -> sum_mem
-
-  naive version. takes 20 seconds per loop on a busy server = 80% cpu usage
-  """
-  indent = "  "
-  sum_cpu = 0
-  sum_mem = 0
-  sum_rss = 0
-  children = []
-  try:
-    with process.oneshot():
-      cpu_times = process.cpu_times()
-      memory_info = process.memory_info()
-      rss = memory_info.rss
-      #cpu = process.cpu_percent(interval=None) # FIXME always zero
-      mem = process.memory_percent()
-      cpu = 0
-      if process.pid in last_cpu_time:
-        #cpu = (cpu_times.user - last_cpu_time[process.pid]) / config_interval * 100 # load in percent
-        cpu = (cpu_times.user - last_cpu_time[process.pid]) / config_interval # load
-      # add self to sum
-      sum_cpu += cpu
-      sum_mem += mem
-      sum_rss += rss
-      last_cpu_time[process.pid] = cpu_times.user
-      for p in process.children():
-        child_tree = None
-        try:
-          # recursion
-          child_tree = get_process_tree_slow(p, depth + 1)
-          #_pid, _name, _cmdline, _cpu, _mem, _sum_cpu, _sum_mem, _children = get_process_tree(p, depth + 1)
-        except psutil.NoSuchProcess as e:
-          continue
-        # add children to sum
-        sum_cpu += child_tree[6]
-        sum_mem += child_tree[7]
-        sum_rss += child_tree[8]
-        children.append(child_tree)
-      pid = process.pid
-      name = process.name()
-      cmdline = process.cmdline()
-      process_tree = (pid, name, cmdline, cpu, mem, rss, sum_cpu, sum_mem, sum_rss, children)
-      return process_tree
-  except psutil.NoSuchProcess as e:
-    raise e
-
 
 ps_fields = ['pid', 'ppid', 'name', 'exe', 'cmdline', 'cwd', 'environ', 'status', 'cpu_times', 'cpu_percent', 'memory_percent', 'memory_info']
 
-def get_process_info(root_process):
 
-  # on a busy machine:
-  # cpu usage: 10% to 20%
-  # loop time: 1sec to 2sec
+def get_process_info(root_process):
 
   process_info = dict()
 
@@ -130,11 +75,6 @@ def get_process_info(root_process):
       process_info[pid]["sum_cpu"] = process_info[pid]["cpu_percent"]
       process_info[pid]["sum_mem"] = process_info[pid]["memory_percent"]
       process_info[pid]["sum_rss"] = process_info[pid]["memory_info"].rss
-
-      #cpu = 0
-      #if process.pid in last_cpu_time:
-      #  #cpu = (cpu_times.user - last_cpu_time[process.pid]) / config_interval * 100 # load in percent
-      #  cpu = (cpu_times.user - last_cpu_time[process.pid]) / config_interval # load
 
       continue
 
@@ -161,24 +101,6 @@ def get_process_info(root_process):
   return process_info
 
 
-def print_process_tree(process_tree, file=sys.stdout, depth=0):
-  if depth == 0:
-    t = time.strftime("%F %T %z")
-    #print(f"\n{'load':<{cpu_width}s} mem rss  vms  proc @ {t}", file=file)
-    print(f"\n{'load':<{cpu_width}s} mem rss  proc @ {t}", file=file)
-    #print(f"\n{'load':<{cpu_width}s} mem proc @ {t}", file=file)
-  (pid, name, cmdline, cpu, mem, rss, sum_cpu, sum_mem, sum_rss, children) = process_tree
-  indent = "  "
-  #print(f"{sum_cpu:3.0f} {sum_mem:3.0f} {indent * depth}{shlex.join(cmdline)}", file=file)
-  #print(f"{sum_cpu:{cpu_width}.0f} {sum_mem:3.0f} {indent * depth}{name}", file=file)
-  #print(f"{sum_cpu:{cpu_width}.1f} {sum_mem:3.0f} {Float(rss):4.0h} {Float(vms):4.0h} {indent * depth}{name}", file=file)
-  # rss is more precise than mem. mem = rss / total_memory
-  print(f"{sum_cpu:{cpu_width}.1f} {sum_mem:3.0f} {Float(sum_rss):4.0h} {indent * depth}{name}", file=file)
-  #print(f"{sum_cpu:{cpu_width}.1f} {sum_mem:3.0f} {indent * depth}{name}", file=file)
-  depth += 1
-  for _process_tree in children:
-    print_process_tree(_process_tree, file, depth)
-
 def cumulate_process_info(process_info, root_pid):
   # depth first
   for child_pid in process_info[root_pid]["child_pids"]:
@@ -199,19 +121,17 @@ def print_process_info(process_info, root_pid, file=sys.stdout, depth=0):
   indent = "  "
   info = process_info[root_pid]
   sum_cpu = info["sum_cpu"] / 100 # = load
-  #mem = info["sum_mem"]
   sum_mem = info["sum_mem"]
   sum_rss = info["sum_rss"]
   name = info["name"]
   cmdline = info["cmdline"]
-  # value None = psutil.AccessDenied
+  # value None == psutil.AccessDenied
   exe = info["exe"] # always None
   cwd = info["cwd"] # always None
   environ = info["environ"] # always None
   child_procs = len(info["child_pids"])
   if len(cmdline) > 0:
     cmdline[0] = os.path.basename(cmdline[0]) # full path is in info["exe"]
-    #if cmdline[0] in {"g++", "gcc", "cc1plus", "as"}:
     if cmdline[0] in {"g++", "gcc"}: # TODO fix other verbose commands
       # make gcc less verbose
       cmdline_short = []
@@ -220,7 +140,6 @@ def print_process_info(process_info, root_pid, file=sys.stdout, depth=0):
         if skip_value:
           skip_value = False
           continue
-        # TODO "-MQ",
         if arg in {"-I", "-B", "-D", "-U", "-isystem", "-idirafter", "--param", "-MF", "-dumpdir", "-dumpbase", "-dumpbase-ext"}:
           # -isystem is the most frequent
           skip_value = True
@@ -236,8 +155,9 @@ def print_process_info(process_info, root_pid, file=sys.stdout, depth=0):
         cmdline_short.append(arg)
       cmdline = cmdline_short
 
-    if cmdline[0] in {"g++", "gcc"}:
-      process_info[root_pid]["child_pids"] = [] # hide gcc child procs: cc1plus, as, ...
+    if cmdline[0] in {"g++", "gcc", "stress-ng"}:
+      # hide child procs
+      process_info[root_pid]["child_pids"] = []
   # TODO print cwd only when different from parent process
   log_info = {"child_procs": child_procs, "cmdline": cmdline}
   if depth == 0:
