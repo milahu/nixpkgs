01bdccb2a2b74b2efb29b5b03ceaa387f6ae8754
Milan Hauth <milahu@gmail.com>
2022-06-25 16:49:32 +0200

    ninja-tokenpool: fix setupHook

--- pkgs/development/tools/build-managers/ninja/ninja-tokenpool.nix
+++ pkgs/development/tools/build-managers/ninja/ninja-tokenpool.nix
@@ -14,6 +14,8 @@
     sha256 = "sha256-cxtmBptmqxFyEXpgFNs7extyDfZBrJKMy1hsIqzruIc=";
   };
 
+  setupHook = ./setup-hook-tokenpool.sh;
+
   meta = with lib; {
     description = "ninja build system with jobserver and jobclient";
     longDescription = ''
