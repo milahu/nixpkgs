4d60a86d45882646397d5832bf822aca04c7e582
Milan Hauth <milahu@gmail.com>
2022-06-25 17:29:24 +0200

    ninja-tokenpool: rename phases

--- pkgs/development/tools/build-managers/ninja/setup-hook-tokenpool.sh
+++ pkgs/development/tools/build-managers/ninja/setup-hook-tokenpool.sh
@@ -1,4 +1,4 @@
-ninjaBuildPhase() {
+ninjaTokenpoolBuildPhase() {
     runHook preBuild
 
     local buildCores=1
@@ -31,10 +31,10 @@ ninjaBuildPhase() {
 }
 
 if [ -z "${dontUseNinjaBuild-}" -a -z "${buildPhase-}" ]; then
-    buildPhase=ninjaBuildPhase
+    buildPhase=ninjaTokenpoolBuildPhase
 fi
 
-ninjaInstallPhase() {
+ninjaTokenpoolInstallPhase() {
     runHook preInstall
 
     # shellcheck disable=SC2086
@@ -53,10 +53,10 @@ ninjaInstallPhase() {
 }
 
 if [ -z "${dontUseNinjaInstall-}" -a -z "${installPhase-}" ]; then
-    installPhase=ninjaInstallPhase
+    installPhase=ninjaTokenpoolInstallPhase
 fi
 
-ninjaCheckPhase() {
+ninjaTokenpoolCheckPhase() {
     runHook preCheck
 
     if [ -z "${checkTarget:-}" ]; then
@@ -91,5 +91,5 @@ ninjaCheckPhase() {
 }
 
 if [ -z "${dontUseNinjaCheck-}" -a -z "${checkPhase-}" ]; then
-    checkPhase=ninjaCheckPhase
+    checkPhase=ninjaTokenpoolCheckPhase
 fi
