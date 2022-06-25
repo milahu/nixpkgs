baef13205bcb5b246f9dd7922bda03b7412ce2e4
Milan Hauth <milahu@gmail.com>
2022-06-25 13:53:30 +0200

    nix_build_profiler: print exe

--- pkgs/development/python-modules/nix-build-profiler/src/nix_build_profiler.py
+++ pkgs/development/python-modules/nix-build-profiler/src/nix_build_profiler.py
@@ -192,7 +192,7 @@ def print_process_info(process_info, root_pid, file=sys.stdout, depth=0):
     parent_cwd = process_info[info["ppid"]]["cwd"]
     if cwd != parent_cwd:
       log_info["cwd"] = cwd
-  #log_info["exe"] = exe
+  log_info["exe"] = exe
   #if depth == 0:
   #  log_info["environ"] = environ # spammy
   info_str = ""
