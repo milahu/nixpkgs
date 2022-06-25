e033d1d8bc87dde3890d2df486580354372d4dcf
Milan Hauth <milahu@gmail.com>
2022-06-25 13:51:39 +0200

    ninja-kitware: init at unstable-2021-05-25

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15907,6 +15907,8 @@ with pkgs;
 
   ninja = callPackage ../development/tools/build-managers/ninja { };
 
+  ninja-kitware = callPackage ../development/tools/build-managers/ninja/ninja-kitware.nix { };
+
   nimbo = with python3Packages; callPackage ../applications/misc/nimbo { };
 
   gn = callPackage ../development/tools/build-managers/gn { };
