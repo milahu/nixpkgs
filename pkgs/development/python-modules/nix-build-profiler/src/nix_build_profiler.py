8ba7798caf5f7f09bf99a5b3744d47b036469d4b
Milan Hauth <milahu@gmail.com>
2022-06-27 11:36:54 +0200

    fix nix_build_profiler.py

--- pkgs/development/python-modules/nix-build-profiler/src/nix_build_profiler.py
+++ pkgs/development/python-modules/nix-build-profiler/src/nix_build_profiler.py
@@ -248,7 +248,7 @@ def print_process_info(process_info, root_pid, file=sys.stdout, depth=0):
     # list file descriptors of process
     cmd_str = f"ls -l /proc/{root_pid}/fd/"
     print(f"$ {cmd_str}", file=file)
-    cmd_out = subprocess.check_output(c, shell=True, stderr=subprocess.STDOUT)
+    cmd_out = subprocess.check_output(cmd_str, shell=True, stderr=subprocess.STDOUT)
     file.write(cmd_out)
 
   # recursion
