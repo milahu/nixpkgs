88e0a105cc70c9ecd145de9daff94b13ed6d3f88
Milan Hauth <milahu@gmail.com>
2022-06-27 09:18:29 +0200

    qtwebengine: debug: use local gnumake-jobclient-js

---
+++ pkgs/development/libraries/qt-6/modules/gnumake-jobclient-js/JobClient.d.ts
@@ -0,0 +1,12 @@
+declare module '@milahu/gnumake-jobclient';
+
+export function JobClient(): JobClient | null;
+
+export type JobClient = {
+    acquire: () => number | null,
+    release: (token: number) => boolean,
+    // read-only properties
+    maxJobs: () => number,
+    maxLoad: () => number | undefined,
+    numTokens: () => number,
+};
