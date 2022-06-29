115e6dd39ca87c83f2a3536daa3120f88e0c88f2
Milan Hauth <milahu@gmail.com>
2022-06-29 15:40:24 +0200

    qtwebengine: add patch: fix inherit fds: devtools-frontend build_inspector_overlay.py

--- pkgs/development/libraries/qt-6/modules/qtwebengine.nix
+++ pkgs/development/libraries/qt-6/modules/qtwebengine.nix
@@ -142,6 +142,8 @@ qtModule rec {
     ./patches/qtwebengine/0007-debug-chromium-node.py.patch
     ./patches/qtwebengine/0008-fix-node-py-for-jobclient.patch # TODO remove. depends on 7
     ./patches/qtwebengine/0009-debug-node-py-print-live-output.patch # depends on 8
+
+    ./patches/qtwebengine/0010-fix-inherit-fds-devtools-frontend-build_inspector_overlay.py.patch
   ];
 
   DEBUG_JEST_WORKER = "1";
