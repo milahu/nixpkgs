97d4ed1bd61a9f56e16230b114c9bac3a6c60f16
Milan Hauth <milahu@gmail.com>
2022-07-03 13:59:16 +0200

    qtwebengine add 0028-rollup-trace-all-function-calls.patch

--- pkgs/development/libraries/qt-6/modules/qtwebengine.nix
+++ pkgs/development/libraries/qt-6/modules/qtwebengine.nix
@@ -126,6 +126,7 @@ qtModule rec {
     ./patches/qtwebengine/0015-fixup-blink-task_queue.py.patch
     ./patches/qtwebengine/0021-fix-blink-task_queue.py-this-self.patch
 
+    # node.py -> rollup, tsc, ...
     ./patches/qtwebengine/0007-debug-chromium-node.py.patch
     ./patches/qtwebengine/0008-fix-node-py-for-jobclient.patch # TODO remove. depends on 7
     ./patches/qtwebengine/0009-debug-node-py-print-live-output.patch # depends on 8
@@ -138,6 +139,7 @@ qtModule rec {
     # FIXME node.py / rollup / jest-worker
     # many rollup's, but zero cpu load
     # rollup fails to start the first worker? -> bug in jest-worker?
+    ./patches/qtwebengine/0028-rollup-trace-all-function-calls.patch
 
     # qtwebengine-everywhere-src-6.3.1 $ grep -r -F 'subprocess.Popen(' | grep -v -e test -e tools
     ./patches/qtwebengine/0010-fix-inherit-fds-devtools-frontend-build_inspector_overlay.py.patch
