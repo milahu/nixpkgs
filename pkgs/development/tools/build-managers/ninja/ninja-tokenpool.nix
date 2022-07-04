565cf1a87ba78520f238da1cf0df91718ddea588
Milan Hauth <milahu@gmail.com>
2022-07-04 12:47:15 +0200

    ninja-tokenpool cleanup

--- pkgs/development/tools/build-managers/ninja/ninja-tokenpool.nix
+++ pkgs/development/tools/build-managers/ninja/ninja-tokenpool.nix
@@ -7,21 +7,11 @@
   pname = "ninja-tokenpool";
   version = "unstable-2022-02-13";
 
-  /*
   src = fetchFromGitHub {
     owner = "stefanb2";
     repo = "ninja";
     rev = "15bc8f783b63e9bf91080d9b9d6c83468f012f97";
     sha256 = "sha256-cxtmBptmqxFyEXpgFNs7extyDfZBrJKMy1hsIqzruIc=";
-  };
-  */
-
-  src = fetchFromGitHub {
-    # https://github.com/milahu/ninja/tree/topic-tokenpool-master-patch-1
-    owner = "milahu";
-    repo = "ninja";
-    rev = "e54e34f14b3b56acc474e6cfd7a64673ed2ed289";
-    sha256 = "u61W036KkPuaXPsRPWO5KqnY7cKB3Hnsr8bR7QAcaP8=";
   };
 
   setupHook = ./setup-hook-tokenpool.sh;
@@ -29,27 +19,11 @@
   meta = with lib; {
     description = "ninja build system with jobserver and jobclient";
     longDescription = ''
-      run `ninja --tokenpool-master` to start the root ninja process
-
-      tokenpool-master = jobserver
-      child ninja processes = jobclients
-
-      all child ninja processes
-      should receive an environemnt variable like
-      MAKEFLAGS="--jobserver-auth=3,4"
-      where 3 and 4 are file-descriptor numbers
-      for read and write access to the jobserver
-
-      debugging:
-      echo $MAKEFLAGS
-      ls -nl /proc/self/fd/
+      run `ninja --tokenpool-master` to start the main build process
     '';
-    # TODO MAKEFLAGS or NINJAFLAGS?
     homepage = "https://github.com/stefanb2/ninja/tree/topic-tokenpool-master";
     # via https://github.com/ninja-build/ninja/issues/1139#issuecomment-461750356
-    # see also
-    # https://gitlab.kitware.com/cmake/cmake/-/issues/21597
-    # https://discourse.cmake.org/t/efficiency-issues-with-externalproject-sub-builds-under-ninja/3241
+    # see also https://gitlab.kitware.com/cmake/cmake/-/issues/21597
     license = licenses.asl20;
     platforms = platforms.unix;
     maintainers = with maintainers; [ milahu ];
