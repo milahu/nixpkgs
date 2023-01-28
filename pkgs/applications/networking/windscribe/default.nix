a8f5a508f94f51b4ac07835431648849d53f4499
Milan Hauth <milahu@gmail.com>
2023-01-28 20:59:41 +0100

    windscribe build from source

--- pkgs/applications/networking/windscribe/default.nix
+++ pkgs/applications/networking/windscribe/default.nix
@@ -1,68 +1,133 @@
-{ stdenv, lib, fetchurl, dpkg
-, alsa-lib, atk, cairo, cups, dbus, expat, fontconfig, freetype
-, gdk-pixbuf, glib, pango, nspr, nss, gtk3, mesa
-, xorg, autoPatchelfHook, systemd, libnotify, libappindicator
+{ stdenv
+, lib
+, alsa-lib
+, atk
+, boost
+, cairo
+, cups
+, dbus
+, expat
+, fontconfig
+, freetype
+, gdk-pixbuf
+, glib
+, pango
+, nspr
+, nss
+, gtk3
+#, qt6
+, qt5
+, mesa
+, xorg
+, autoPatchelfHook
+, systemd
+, libnotify
+, libappindicator
 , makeWrapper
+, fetchFromGitHub
+, wrapGAppsHook
+, python3
+, wrapQtAppsHook
 }:
 
-let deps = [
-  alsa-lib
-  atk
-  cairo
-  cups
-  dbus
-  expat
-  fontconfig
-  freetype
-  gdk-pixbuf
-  glib
-  pango
-  gtk3
-  libappindicator
-  libnotify
-  mesa
-  xorg.libX11
-  xorg.libXScrnSaver
-  xorg.libXcomposite
-  xorg.libXcursor
-  xorg.libXdamage
-  xorg.libXext
-  xorg.libXfixes
-  xorg.libXi
-  xorg.libXrandr
-  xorg.libXrender
-  xorg.libXtst
-  xorg.libxcb
-  xorg.libxshmfence
-  nspr
-  nss
-  systemd
-];
+let
+  deps = [
+    alsa-lib
+    atk
+    cairo
+    boost
+    cups
+    dbus
+    expat
+    fontconfig
+    freetype
+    gdk-pixbuf
+    glib
+    pango
+    gtk3
+    #qt6.qtbase
+    qt5.qtbase
+    libappindicator
+    libnotify
+    mesa
+    nspr
+    nss
+    systemd
+    #python # error: pathspec-0.10.1 not supported for interpreter python2.7
+    python3
+  ] ++ (with xorg; [
+    libX11
+    libXScrnSaver
+    libXcomposite
+    libXcursor
+    libXdamage
+    libXext
+    libXfixes
+    libXi
+    libXrandr
+    libXrender
+    libXtst
+    libxcb
+    libxshmfence
+  ]) ++ (with python3.pkgs; [
+    # tools/requirements.txt
+    colorama
+    glob2
+    pyyaml
+  ]);
 in
 stdenv.mkDerivation rec {
   pname = "windscribe";
   version = "2.4.11";
 
-  src = fetchurl {
-    url = "https://github.com/Windscribe/Desktop-App/releases/download/v${version}/windscribe_${version}_amd64.deb";
-    sha256 = "sha256-ovQU8gMltPeJi3vXH+FI3eMOEMm4vxVQ+yrlN96rNAE=";
+  src = fetchFromGitHub {
+    owner = "Windscribe";
+    repo = "Desktop-App";
+    rev = "v${version}";
+    sha256 = "sha256-2aFeF/gT3y1VQhg5ZzqS5iyCxgvOkiYtDOM+woSeMwc=";
   };
 
+  postPatch = ''
+    echo removing binary files
+    rm -rfv backend/windows/ installer/windows/
+    find . '(' -name '*.exe' -or -name '*.dll' ')' | xargs rm -v
+    grep -rIL . | grep -v -E '.(png|jpg|tiff|ico|icns|ttf)$' | xargs rm -v
+  '';
+
+  /*
+    echo migrating scripts to python3
+    2to3 -w -n .
+  */
+
   nativeBuildInputs = [
-    autoPatchelfHook
-    dpkg
-    makeWrapper
+    #autoPatchelfHook
+    #makeWrapper
+    #wrapGAppsHook
+    wrapQtAppsHook
   ];
 
   buildInputs = deps;
 
-  dontBuild = true;
-  dontConfigure = true;
+  #dontBuild = true;
+  #dontConfigure = true;
 
-  unpackPhase = "dpkg-deb -x $src .";
+  #runtimeDependencies = [ (lib.getLib systemd) libnotify libappindicator ];
 
-  runtimeDependencies = [ (lib.getLib systemd) libnotify libappindicator ];
+  # this is just a complex wrapper for qmake
+  /*
+    pushd tools
+    python3 build_all.py
+    popd
+  */
 
+  buildPhase = ''
+    pushd backend/linux/helper
+    qmake # generate Makefile
+    make
+    popd
+  '';
+
+  /*
   installPhase = ''
     runHook preInstall
 
@@ -81,12 +146,13 @@ stdenv.mkDerivation rec {
 
     runHook postInstall
   '';
+  */
 
   meta = with lib; {
-    homepage = "https://windscribe.net";
+    homepage = "https://github.com/Windscribe/Desktop-App";
     description = "Client for windscribe VPN";
     license = licenses.gpl2Only;
-    platforms = [ "x86_64-linux" ];
+    #platforms = [ "x86_64-linux" ];
     maintainers = with maintainers; [ arphe42 ];
   };
 }
