7d9308bd1701e28e4b747645e2bed4cd079a9105
Milan Hauth <milahu@gmail.com>
2023-01-29 16:58:03 +0100

    fix

--- pkgs/development/tools/analysis/radare2/plugins/r2ghidra/default.nix
+++ pkgs/development/tools/analysis/radare2/plugins/r2ghidra/default.nix
@@ -66,6 +66,13 @@ clangStdenv.mkDerivation rec {
     chmod -R +w source/ghidra/src
   '';
 
+  postPatch = ''
+    substituteInPlace src/SleighAsm.cpp \
+      --replace \
+        'path = strdup (R2_PREFIX "/lib/radare2/" R2_VERSION "/r2ghidra_sleigh");' \
+        'path = strdup ("'$out'" "/lib/radare2/" R2_VERSION "/r2ghidra_sleigh");'
+  ''
+
   NIX_CFLAGS_COMPILE = [
     "-O1" # fortify
     # hide warnings
