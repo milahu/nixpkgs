bca08c45d7342760b6d2bb859f6252cba4758d9d
Milan Hauth <milahu@gmail.com>
2023-02-02 11:59:31 +0100

    frida-v8: init at unstable-2022-11-02

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7352,6 +7352,7 @@ with pkgs;
   };
 
   frida-gum = callPackage ../development/tools/analysis/frida/frida-gum { };
+  frida-v8 = callPackage ../development/tools/analysis/frida/frida-v8 { };
 
   frostwire = callPackage ../applications/networking/p2p/frostwire { };
   frostwire-bin = callPackage ../applications/networking/p2p/frostwire/frostwire-bin.nix { };
