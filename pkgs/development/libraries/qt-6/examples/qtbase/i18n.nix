fc3ecd3a7bdcfa6f77e06e19c95991c101d84f71
Milan Hauth <milahu@gmail.com>
2023-02-10 09:43:13 +0100

    fix-qtbase-ex

--- pkgs/development/libraries/qt-6/examples/qtbase/i18n.nix
+++ pkgs/development/libraries/qt-6/examples/qtbase/i18n.nix
@@ -8,14 +8,16 @@
 
 stdenv.mkDerivation rec {
   pname = "qtbase-example-i18n";
+  # src is a tar.xz file
   inherit (qtbase) version src;
   prePatch = ''
     cd examples/widgets/tools/i18n
   '';
   # tries install to ${qtbase.out}/examples/
   installPhase = ''
-    mkdir -p $out/bin
-    cp i18n $out/bin
+    runHook preInstall
+    install -Dm 555 i18n $out/bin/i18n
+    runHook postInstall
   '';
   nativeBuildInputs = [
     qmake
