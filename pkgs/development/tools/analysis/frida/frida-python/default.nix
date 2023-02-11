762c1fe1cfc93742b47212593a9351e470f5cdff
Milan Hauth <milahu@gmail.com>
2023-02-11 18:06:19 +0100

    frida-python: fix build

--- pkgs/development/tools/analysis/frida/frida-python/default.nix
+++ pkgs/development/tools/analysis/frida/frida-python/default.nix
@@ -7,8 +7,6 @@
 , pkg-config
 , frida-core
 , python3
-, nodejs-19_x
-, callPackage
 }:
 
 let
@@ -24,47 +22,13 @@ stdenv.mkDerivation rec {
     meson
     pkg-config
     cmake
-    #ninja
+    ninja
   ];
 
   buildInputs = [
     frida-core
-    python3 # agents/build.py
-    #nodejs-19_x # npm, same nodejs version as frida-gum
+    python3
   ];
-
-/*
-  postPatch = ''
-    chmod +x agents/build.py
-    patchShebangs .
-    substituteInPlace agents/build.py \
-      --replace 'capture_output=True' 'capture_output=False' \
-      --replace 'subprocess.run([npm, "install"]' 'pass #' \
-
-  '';
-
-  preBuild = ''
-    # agents/build.py -> priv_dir
-    mkdir -p agents/fs/fs_agent.js.p
-    pushd agents/fs/fs_agent.js.p
-    cp -r ${frida-fs-agent.nodeDependencies}/lib/node_modules .
-    chmod -R +w node_modules
-    #export PATH=$PWD/node_modules/.bin:$PATH
-    popd
-
-    # agents/build.py -> priv_dir
-    mkdir -p agents/tracer/tracer_agent.js.p
-    pushd agents/tracer/tracer_agent.js.p
-    cp -r ${frida-tracer-agent.nodeDependencies}/lib/node_modules .
-    chmod -R +w node_modules
-    #export PATH=$PWD/node_modules/.bin:$PATH
-    popd
-  '';
-
-  passthru = {
-    inherit frida-fs-agent frida-tracer-agent;
-  };
-*/
 
   meta = with lib; {
     description = "Python bindings for Frida";
