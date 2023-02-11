0452c29ed51bb911b0bef056a48f5304ccdf859c
Milan Hauth <milahu@gmail.com>
2023-02-11 17:30:53 +0100

    frida-tools: first build, missing python deps: frida, colorama, ...

--- pkgs/development/tools/analysis/frida/frida-tools/default.nix
+++ pkgs/development/tools/analysis/frida/frida-tools/default.nix
@@ -10,6 +10,9 @@
 , ninja
 , pkg-config
 , frida-core
+, python3
+, nodejs-19_x
+, callPackage
 /*
 , frida-vala
 , frida-glib
@@ -21,14 +24,21 @@
 , libsoup_3
 , brotli
 , libnice
-, python3
-, nodejs-19_x
-, callPackage
 */
 }:
 
 let
   srcs = builtins.fromJSON (builtins.readFile ../srcs.json);
+
+  # node2nix -d -l package-lock.json
+  frida-fs-agent = callPackage ./agents/fs {
+    nodejs = nodejs-19_x;
+  };
+
+  # node2nix -d -l package-lock.json
+  frida-tracer-agent = callPackage ./agents/tracer {
+    nodejs = nodejs-19_x;
+  };
 in
 
 stdenv.mkDerivation rec {
@@ -46,6 +56,8 @@ stdenv.mkDerivation rec {
 
   buildInputs = [
     frida-core
+    python3 # agents/build.py
+    nodejs-19_x # npm, same nodejs version as frida-gum
     /*
     frida-gum
     frida-glib
@@ -57,8 +69,6 @@ stdenv.mkDerivation rec {
     libsoup_3
     brotli
     libnice
-    python3 # src/compiler/generate-agent.py
-    nodejs-19_x # npm, same nodejs version as frida-gum
     */
   ];
 
@@ -73,37 +83,51 @@ stdenv.mkDerivation rec {
     */
   ];
 
-/*
   postPatch = ''
+    chmod +x agents/build.py
     patchShebangs .
-    substituteInPlace src/compiler/generate-agent.py \
+    substituteInPlace agents/build.py \
       --replace 'capture_output=True' 'capture_output=False' \
       --replace 'subprocess.run([npm, "install"]' 'pass #' \
-      --replace 'with urllib.request.urlopen' '#' \
-      --replace '        (output_dir / "node_modules" / "@types" / "frida-gum"' '#' \
-      --replace '    shutil.copyfileobj(response, frida_gum_types)' '#' \
 
   '';
-
+/*
   # https://github.com/frida/v8/issues/14
   preConfigure = ''
     export PATH=${frida-v8}/bin/linux-x86_64:$PATH
   '';
 
-  preBuild = ''
     mkdir -p src/compiler
     pushd src/compiler
 
-    cp -r ${frida-compiler-agent.nodeDependencies}/lib/node_modules .
+  */
+
+  preBuild = ''
+    # agents/build.py -> priv_dir
+    mkdir -p agents/fs/fs_agent.js.p
+    pushd agents/fs/fs_agent.js.p
+    cp -r ${frida-fs-agent.nodeDependencies}/lib/node_modules .
     chmod -R +w node_modules
-    export PATH=$PWD/node_modules/.bin:$PATH
+    #export PATH=$PWD/node_modules/.bin:$PATH
+    popd
 
-    mkdir -p node_modules/@types/frida-gum
-    cp ${src-frida-gum-dts} node_modules/@types/frida-gum/index.d.ts
-
+    # agents/build.py -> priv_dir
+    mkdir -p agents/tracer/tracer_agent.js.p
+    pushd agents/tracer/tracer_agent.js.p
+    cp -r ${frida-tracer-agent.nodeDependencies}/lib/node_modules .
+    chmod -R +w node_modules
+    #export PATH=$PWD/node_modules/.bin:$PATH
     popd
   '';
-  */
+
+/*
+    mkdir -p node_modules/@types/frida-gum
+    cp ${src-frida-gum-dts} node_modules/@types/frida-gum/index.d.ts
+*/
+
+  passthru = {
+    inherit frida-fs-agent frida-tracer-agent;
+  };
 
   meta = with lib; {
     description = "Dynamic instrumentation toolkit for developers, reverse-engineers, and security researchers";
