a822d5b0b8b5915238eb73e664b9c1ddef94d226
milahu <milahu@gmail.com>
2023-02-19 17:31:57 +0100

    frida-python fix

--- pkgs/development/tools/analysis/frida/frida-python/default.nix
+++ pkgs/development/tools/analysis/frida/frida-python/default.nix
@@ -50,6 +50,12 @@ stdenv.mkDerivation rec {
     libsoup_3
   ];
 
+  mesonFlags = [
+      # based on github CI of https://github.com/frida/frida
+      "-Doptimization=s"
+      "-Db_ndebug=true"
+    ];
+
   meta = with lib; {
     description = "Python bindings for Frida";
     homepage = "https://github.com/frida/frida-python";
