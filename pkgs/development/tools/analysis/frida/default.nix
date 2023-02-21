3b4abbc54285d241977b494ffc6a506c06144acf
milahu <milahu@gmail.com>
2023-02-21 18:02:49 +0100

    frida: init for debug

---
+++ pkgs/development/tools/analysis/frida/default.nix
@@ -0,0 +1,184 @@
+{ lib
+, stdenv
+, fetchFromGitHub
+, fetchpatch
+, meson
+, pkg-config
+, cmake
+, ninja
+, frida-glib
+#, glib
+, frida-glib-networking
+, frida-tinycc
+, frida-v8
+, frida-quickjs
+, capstone_5
+, lzma
+, gobject-introspection
+, libunwind
+, libelf
+, libdwarf
+, json-glib
+, sqlite
+, libsoup_3
+, python3
+, nodePackages
+, libffi
+, enableGumjs ? true # Build JavaScript bindings
+, enableGumpp ? true # Build C++ bindings
+, git
+, which
+, perl
+, nodejs
+, frida-gum
+}:
+
+let
+  srcs = builtins.fromJSON (builtins.readFile ./srcs.json);
+in
+
+stdenv.mkDerivation rec {
+  pname = "frida";
+  inherit (srcs) version;
+  src = fetchFromGitHub srcs.paths.${pname}.github;
+
+  patchPhase = ''
+    runHook prePatch
+    pushd frida-gum
+    for patch in ${frida-gum.patches}; do
+      echo frida-gum: applying patch $patch
+      patch -p1 < $patch
+    done
+    echo frida-gum: patching bindings/gumjs/generate-runtime.py
+    substituteInPlace bindings/gumjs/generate-runtime.py \
+      --replace 'capture_output=True' 'capture_output=False' \
+      --replace \
+        'frida_compile = output_dir / "node_modules" / ".bin" / make_script_filename("frida-compile")' \
+        'frida_compile = Path("${nodePackages.frida-compile}/bin/frida-compile")'
+    popd
+    patchShebangs .
+    runHook postPatch
+  '';
+  # TODO patch "ldd /bin/ls" calls in Makefile
+
+  buildPhase = ''
+    runHook preBuild
+    make gum-linux-x86_64
+    runHook postBuild
+  '';
+
+  nativeBuildInputs = [];
+
+  # no effect. wtf?
+  #makeTargets = [ "gum-linux-x86_64" ];
+
+  /*
+  mesonFlags = [
+      # based on github CI of https://github.com/frida/frida
+      "-Ddefault_library=static"
+      "-Doptimization=s"
+      "-Db_ndebug=true"
+      #"-Djailbreak=auto"
+      "-Ddatabase=enabled"
+      #"-Dfrida_objc_bridge=auto"
+      #"-Dfrida_swift_bridge=auto"
+      #"-Dfrida_java_bridge=auto"
+      "-Dtests=enabled"
+      #"-Dtests=disabled"
+      # FIXME tests break
+      # blame "-Dquickjs=disabled"?
+      # undefined reference to gum_quick_script_backend_get_type
+      # https://github.com/frida/frida-gum/issues/723
+    ]
+    ++ lib.optionals enableGumjs [
+      "-Dgumjs=enabled"
+      #"-Dquickjs=enabled"
+      #"-Dquickjs=disabled"
+      #"-Dv8=enabled"
+    ]
+    ++ lib.optionals enableGumpp [
+      "-Dgumpp=enabled"
+    ];
+  */
+
+  #dontConfigure = true; # force not using meson configure phase
+
+  preBuild = ''
+    # force not using hooks. WTF? hooks should ne be used of buildInputs
+    # force not using meson configure phase
+    export PATH=${meson}/bin:$PATH
+    # force not using ninja build phase
+    export PATH=${ninja}/bin:$PATH
+    export PATH=${cmake}/bin:$PATH
+    git init
+    git add Makefile
+    export HOME=$TMP
+    git config --global init.defaultBranch main
+    git config --global user.email nixbld@localhost
+    git config --global user.name nixbld
+    git commit -m init
+  '';
+
+  buildInputs = [
+    /*
+    meson
+    pkg-config
+    cmake
+    ninja
+    */
+
+    git
+    which
+    perl
+    nodejs
+
+    # undefined reference to gum_quick_script_backend_get_type
+    # https://github.com/frida/frida-gum/issues/723
+    # quickfix: disable tests
+    frida-glib
+    #glib
+    frida-glib-networking
+    capstone_5
+    lzma
+    libunwind
+    libelf
+    libdwarf
+    gobject-introspection # g-ir-scanner
+  ] ++ lib.optionals enableGumjs [
+    frida-tinycc
+    frida-v8
+    frida-quickjs
+    json-glib
+    sqlite
+    libsoup_3
+    python3 # generate-bindings.py
+  ];
+
+  propagatedBuildInputs = [
+    capstone_5
+    libunwind
+    libelf
+    libdwarf
+    frida-glib # gio gio-unix
+    # dont propagate glib
+    # this would break frida-core:
+    # undefined reference to g_thread_garbage_collect
+    # undefined reference to gio_prepare_to_fork
+    # undefined reference to glib_prepare_to_fork
+    #glib # gio gio-unix
+  ] ++ lib.optionals enableGumjs [
+    frida-v8
+    json-glib
+    libffi
+    frida-tinycc # libtcc
+    sqlite # sqlite3
+  ];
+
+  meta = with lib; {
+    description = "instrumentation and introspection library";
+    homepage = "https://github.com/frida/frida";
+    license = licenses.wxWindows;
+    maintainers = with maintainers; [ milahu ];
+    platforms = platforms.unix;
+  };
+}
